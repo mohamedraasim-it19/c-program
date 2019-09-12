1745faeadf7d7c93c76d5f01b6c8e52cc7d6ca5b
mohamedraasim-it19 <53462754+mohamedraasim-it19@users.noreply.github.com>
2019-09-12 12:00:38 +0530

    Create rcb.c

---
+++ rcb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three number");
+scanf("%d%d%d",&a,&b,&c);
+if((a<b)&&(a>c))
+{
+printf("a is greatest");
+}
+else if(b>c)
+{
+printf("b is greatest");
+}
+else
+{
+printf("b is greatest");
+}
+{
+printf("c is greatest");
+}
+}
